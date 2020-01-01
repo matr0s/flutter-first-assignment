b6a2f99c5a7bea5ee06d8410b51d1fbbd5be00a7
Serhii Osadchuk <serhii@Admins-Mac-mini.local>
2020-01-01 12:41:06 +0200

    Init stage

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,26 @@
 // below it
 // 2) Add a button which changes the text (to any other text of your choice)
 // 3) Split the app into three widgets: App, TextControl & Text
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyOwnApp());
+}
+
+class MyOwnApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyOwnApp();
+  }
+}
+
+class _MyOwnApp extends State<MyOwnApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('My first Own App')),
+    ));
+  }
+}
